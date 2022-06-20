2acc70559a26782031dae1abaaf9cf1d21db0eaa
williamjordan <will.jordan0788@gmail.com>
2022-06-19 20:02:13 -0700

    commit tree 20f9d884257bf37833ce60cb485a49e129bd81c4

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solutions in this file
+const names = ["Joe", "Nina", "Eric"]
+
+function writeCards(names, event) {
+   return  names.map(name=> `Thank you, ${name} for the wonderful ${event} gift!`)
+}
+
+console.log(writeCards(names, "birthday"))
